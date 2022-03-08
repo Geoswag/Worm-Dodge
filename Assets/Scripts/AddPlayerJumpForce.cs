3e4389ef8ed8970be4a815ef997b6c868ad02571
Geoswag <100380401+Geoswag@users.noreply.github.com>
2022-03-08 20:50:22 +0000

    Update AddPlayerJumpForce.cs

--- Assets/Scripts/AddPlayerJumpForce.cs
+++ Assets/Scripts/AddPlayerJumpForce.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//OLD MOVEMENT, CURRENTLY UNUSED
+
 public class AddPlayerJumpForce : MonoBehaviour
 {
     [SerializeField] Vector3 v3Force;
