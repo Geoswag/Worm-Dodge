7df11c314f99175bc2113abe114037a971ded609
Geoswag <100380401+Geoswag@users.noreply.github.com>
2022-03-08 20:52:32 +0000

    Update CopyPosition.cs

--- Assets/Scripts/CopyPosition.cs
+++ Assets/Scripts/CopyPosition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Used to keep camera on player without rotation
 public class CopyPosition : MonoBehaviour
 {
     [SerializeField] Transform transTarget;
